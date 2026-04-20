2bc44eea66efc25aa850ef6585708ecf238cf859
Surbhi <surbhisakhiya28@email.com>
2026-04-20 18:12:18 +0530

    initial commit

--- chatapp/client/src/pages/LoginPage.jsx
+++ chatapp/client/src/pages/LoginPage.jsx
@@ -24,8 +24,8 @@ const LoginPage = () => {
    }
     
   return (
-    <div className='min-h-screen bg-cover bg-center flex items-center 
-    justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl'>
+   <div style={{ backgroundImage: `url(${bg})` }}
+  className='min-h-screen bg-cover bg-center flex items-center justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl'>
       {/* -----------left-------------- */}
         <img src={assets.logo_big} alt="" className='w-[min(30vw,250px)]' />
 
